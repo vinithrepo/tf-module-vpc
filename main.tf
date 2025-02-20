b73779473800119154e4a851d89b63cb2d95fb60
Vinith Amma <vinithamma007@gmail.com>
2025-02-20 09:53:06 +0530

    tf practice

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr
   tags = {
-    Name = "main"
+    Name = "test"
   }
+}
+module "subnets" {
+  source = "./subnets"
+  for_each = var.subnets
+  subnets = each.value
+  vpc_id = aws_vpc.main.id
 }
---
+++ subnets/main.tf
@@ -0,0 +1,12 @@
+resource "aws_subnet" "main" {
+  for_each = var.subnets
+  vpc_id     = var.vpc_id
+  cidr_block = each.value["cidr"]
+  availability_zone = each.value["az"]
+  tags = {
+    Name = each.key
+  }
+}
+
+variable "subnets" {}
+variable "vpc_id" {}
