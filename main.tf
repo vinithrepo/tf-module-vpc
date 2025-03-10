b9797ef7267ff08e9c5fdb8b96235cc4eae53868
Vinith Amma <vinithamma007@gmail.com>
2025-03-10 22:46:02 +0530

    tf practice

--- main.tf
+++ main.tf
@@ -21,12 +21,37 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_route" "igw" {
-  for_each = lookup(lookup(module.subnets, "public", null), "routetable_ids", null)
+  for_each = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
   route_table_id            = each.value["id"]
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.igw.id
 }
+resource "aws_eip" "ngw" {
+  #for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  #domain   = "vpc"
+  for_each = local.public_subnet_ids
+  domain = "vpc"
+}
+resource "aws_nat_gateway" "ngw" {
+  #for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  #allocation_id = lookup(lookup(aws_eip.ngw, each.key, null ),"id", null )
+  #subnet_id     = each.value["id"]
+  count = local.public_subnet_ids
+  allocation_id = element(aws_eip.ngw.*.id, count.index )
+  subnet_id = element(local.public_subnet_ids, count.index)
 
+}
+
+resource "aws_route" "ngw" {
+  #for_each = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
+  #route_table_id            = each.value["id"]
+  #destination_cidr_block = "0.0.0.0/0"
+  #gateway_id = aws_nat_gateway.ngw.id
+  count = local.private_route_table_ids
+  route_table_id = element(local.private_route_table_ids, count.index )
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = element(aws_nat_gateway.ngw.*.id, count.index)
+}
 output "subnets" {
   value = module.subnets
 }
--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -1,6 +1,6 @@
 output "subnet_ids" {
   value = aws_subnet.main
 }
-output "routetable_ids" {
+output "route_table_ids" {
   value = aws_route_table.main
 }
