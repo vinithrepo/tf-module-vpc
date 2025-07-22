02e1aa867ddf1bfa76230ddd5972e1568097102e
Vinith Amma <vinithamma007@gmail.com>
2025-07-22 21:42:31 +0530

    app module alb&tg s-42

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr
-  tags = merge(local.tags, {Name = "${var.env}-vpc"})
+  tags       = merge(local.tags, { Name = "${var.env}-vpc" })
 
 }
 
@@ -9,13 +9,13 @@ module "subnets" {
   for_each = var.subnets
   subnets  = each.value
   vpc_id   = aws_vpc.main.id
-  tags = local.tags
-  env = var.env
+  tags     = local.tags
+  env      = var.env
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  tags = merge(local.tags, {Name = "${var.env}-igw"})
+  tags   = merge(local.tags, { Name = "${var.env}-igw" })
 }
 
 resource "aws_route" "igw" {
@@ -38,7 +38,7 @@ resource "aws_nat_gateway" "ngw" {
   count         = length(local.public_subnet_ids)
   allocation_id = element(aws_eip.ngw.*.id, count.index )
   subnet_id     = element(local.public_subnet_ids, count.index)
-  tags = merge(local.tags, {Name = "${var.env}-ngw"})
+  tags          = merge(local.tags, { Name = "${var.env}-ngw" })
 
 }
 
@@ -54,10 +54,10 @@ resource "aws_route" "ngw" {
 }
 
 resource "aws_vpc_peering_connection" "peering" {
-  peer_vpc_id   = aws_vpc.main.id
-  vpc_id        = var.default_vpc_id
-  auto_accept   = true
-  tags = merge(local.tags, {Name = "${var.env}-peering"})
+  peer_vpc_id = aws_vpc.main.id
+  vpc_id      = var.default_vpc_id
+  auto_accept = true
+  tags        = merge(local.tags, { Name = "${var.env}-peering" })
 }
 
 resource "aws_route" "peer" {
